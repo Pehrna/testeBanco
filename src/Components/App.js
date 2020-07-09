c6e407bb3124919a53179d6aec08cdf1076f992c
Pehrna <vikcostas@hotmail.com>
2020-07-09 18:09:08 -0300

    '09/07'

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,23 @@
 import * as React from 'react';
-import { FlatList, StyleSheet, View, Text, TouchableOpacity, Button } from 'react-native';
+import { FlatList, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 
-function HomeScreen({ navigation }) {
+function HomeScreen({ navigation, route }) {
 
-  var projeto = [];
-  const [data, setData] = React.useState([]);
-  if (data.length === 0) {
+  console.log(route)
+
+  var projetoHomeScreen = [];
+  const [dataHome, setData] = React.useState([]);
+  if (dataHome.length === 0) {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then(res => res.json())
       .then(res => {
 
         setData(res);
-        
+
       })
       .catch(err => console.log('Fail', err))
   }
@@ -23,53 +25,34 @@ function HomeScreen({ navigation }) {
   var rowsUser = [];
   var cont = 0;
   React.useEffect(() => {
-    projeto = data;
-    console.log(projeto)
-    console.log(projeto.lenght)
-  
-    for (let i = 0; i < projeto.length; i++) {
+    projetoHomeScreen = dataHome
+    for (let i = 0; i < projetoHomeScreen.length; i++) {
       cont = 0;
       for (let j = 0; j < rowsUser.length; j++) {
-        if (projeto[i].userId === rowsUser[j].numero) {
+        if (projetoHomeScreen[i].userId === rowsUser[j].numero) {
           cont = 1;
         }
       }
       if (cont === 0) {
-        aux = { numero: projeto[i].userId, userId: "Key:" + projeto[i].userId, }
+        aux = { numero: projetoHomeScreen[i].userId, userId: "Key:" + projetoHomeScreen[i].userId, }
         rowsUser.push(aux);
       }
     }
   })
 
-  // var aux = {};
-  // var rowsUser = [];
-  // var cont = 0;
-  // for (let i = 0; i < projeto.length; i++) {
-  //   cont = 0;
-  //   for (let j = 0; j < rowsUser.length; j++) {
-  //     if (projeto[i].userId === rowsUser[j].numero) {
-  //       cont = 1;
-  //     }
-  //   }
-  //   if (cont === 0) {
-  //     aux = { numero: projeto[i].userId, userId: "Key:" + projeto[i].userId, }
-  //     rowsUser.push(aux);
-  //   }
-  // }
-
   return (
 
-    <View style={styles.container} >
+    <View style={styles.containerHome} >
 
       <FlatList
         data={rowsUser}
         renderItem={({ item }) =>
 
-          <View style={styles.line}>
+          <View style={styles.lineHome}>
             <TouchableOpacity onPress={() => {
-              navigation.navigate('Postagens', item)
+              navigation.navigate('Postagens', {parametro: item.userId, state: projetoHomeScreen})
             }} >
-              <Text style={styles.info}>Usuario {item.numero} </Text>
+              <Text style={styles.infoHome}>Usuario {item.numero} </Text>
             </TouchableOpacity>
           </View>
         }
@@ -80,26 +63,81 @@ function HomeScreen({ navigation }) {
   );
 }
 
-function Postagens2({ navigation, route }) {
-  const { numero } = route.params;
-  console.log(route)
-  console.log(JSON.stringify(route))
-  console.log(route.params)
-  console.log(JSON.stringify(route.params))
-  console.log(numero);
-  console.log(JSON.stringify(numero))
+function Postagens({ navigation, route }) {
+  
+  const {parametro} = route.params;
+  const {state} = route.params;
+
+  const [dataPostagem, setDataPostagem] = React.useState(state);
+
+  var rowsUser = [];
+
+    for (let i = 0; i < dataPostagem.length; i++) {          
+      if (JSON.stringify(dataPostagem[i].userId) === parametro.slice(4)) {
+        rowsUser.push(dataPostagem[i]);
+      }
+    }
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Postagens')}
+    <View style={styles.containerPostagem} >
+
+      <FlatList
+        data={rowsUser}
+        renderItem={({ item }) =>
+
+          <View style={styles.linePostagens}>
+            <TouchableOpacity onPress={() => {
+              navigation.navigate('Editar Postagens', {item})
+            }} >
+              {/* Aqui há troca do  (barra N) 
+              <Text style={styles.infoPostagem}>{item.body.replace('\n', '/n')}</Text>*/}
+              <Text style={styles.infoPostagemTitulo}>{item.title}</Text>
+              <Text style={styles.infoPostagem}>{item.body}</Text>
+            </TouchableOpacity>
+          </View>
+        }
+        keyExtractor={item => 'key:'+item.id}
       />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
+
     </View>
   );
+}
+
+function CreatePostagens({ navigation,route }) {
+}
+
+function EditPostagens({ navigation,route }) {
+
+  const {parametro} = route.params;
+  const {state} = route.params;
+
+  const [dataCreatePostagem, setDataCreatePostagem] = React.useState(state);
+
+  var rowsUser = [];
+
+  return (
+    <View style={styles.containerPostagem} >
+
+      <FlatList
+        data={rowsUser}
+        renderItem={({ item }) =>
+
+          <View style={styles.linePostagens}>
+            <TouchableOpacity onPress={() => {
+              navigation.navigate('Editar Postagens', item)
+            }} >
+              {/* Aqui há troca do  (barra N) 
+              <Text style={styles.infoPostagem}>{item.body.replace('\n', '/n')}</Text>*/}
+              <Text style={styles.infoPostagemTitulo}>{item.title}</Text>
+              <Text style={styles.infoPostagem}>{item.body}</Text>
+            </TouchableOpacity>
+          </View>
+        }
+        keyExtractor={item => "key:" + item.id}
+      />
+
+    </View>
+  ); 
 }
 
 const Stack = createStackNavigator();
@@ -112,23 +150,6 @@ export default class App extends React.Component {
     this.state = { data: [] }
   }
 
-  componentDidMount() {
-
-    if (this.state.data.length === 0) {
-      fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(res => res.json())
-        .then(res => {
-
-          this.setState({
-            data: res || []
-          })
-
-        })
-        .catch(err => console.log('Fail', err))
-    }
-  }
-
-
   render() {
 
     //console.log(this.state.data);
@@ -136,22 +157,9 @@ export default class App extends React.Component {
       <NavigationContainer InitiaState={this.state.data}>
         <Stack.Navigator initialRouteName="Home" >
           <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Postagens" component={Postagens2} />
-          {/* <View style={styles.container} >
-
-                        <FlatList
-                            data={rowsUser}
-                            renderItem={({ item }) => (
-
-                                <View style={styles.line}>
-                                    {/* <TouchableOpacity  onPress={this.props.navigation.navigate('Postagens')} >
-                                <Text style={styles.info}>Usuario {item} </Text>
-                            < /TouchableOpacity> }
-                                </View>
-                            )}
-                            keyExtractor={item => "Key:" + item}
-                        />
-                    </View> */}
+          <Stack.Screen name="Postagens" component={Postagens} />
+          <Stack.Screen name="Editar Postagens" component={EditPostagens} />
+          <Stack.Screen name="Criar Postagens" component={CreatePostagens} />
         </Stack.Navigator>
       </NavigationContainer >
     );
@@ -160,7 +168,7 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
 
-  container: {
+  containerHome: {
     width: 360,
     height: 640,
     justifyContent: 'center',
@@ -168,16 +176,42 @@ const styles = StyleSheet.create({
     textAlignVertical: 'center',
     backgroundColor: '#2c3e50'
   },
-  info: {
+  containerPostagem: {
+    width: 360,
+    height: 640,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlignVertical: 'center',
+    backgroundColor: '#2c3e50'
+
+  },
+  infoHome: {
     flexDirection: "column",
     justifyContent: "flex-start",
     color: "white",
     fontSize: 20
 
   },
-  line: {
+  infoPostagem: {
+    //height: 'auto',
+    flexDirection: "column",
+    justifyContent: "justify",
+    color: "white",
+    fontSize: 13,
+    marginLeft: 10
 
-    height: 50,
+  },
+  infoPostagemTitulo: {
+    //height: 'auto',
+    flexDirection: "column",
+    justifyContent: "justify",
+    color: "white",
+    fontSize: 15,
+    fontWeight: 'bold'
+
+  },
+  lineHome: {
+    height: 40,
     width: 300,
     flexDirection: "row",
     borderTopColor: '#ccc',
@@ -186,9 +220,16 @@ const styles = StyleSheet.create({
     borderBottomWidth: 2,
     justifyContent: "center",
     alignItems: "center"
-
-
+  },
+  linePostagens: {
+    width: 340,
+    height: 'auto',
+    flexDirection: "row",
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 2,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    textAlign:'justify'
   }
-
 
 })
